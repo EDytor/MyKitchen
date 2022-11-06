e4c8d248a7ddeaa46c0058dea9088591e97b1f28
EDyTor <91157109+EDyTor@users.noreply.github.com>
2022-11-06 21:04:29 +0100

    Init Commit

---
+++ src/main/java/pl/mykitchen/mykitchen/MyKitchenApplication.java
@@ -0,0 +1,13 @@
+package pl.mykitchen.mykitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKitchenApplication.class, args);
+	}
+
+}
