2f77846d89d936cc726a54deab5669707eff5922
EDyTor <91157109+EDyTor@users.noreply.github.com>
2022-12-02 13:08:14 +0100

    Modified ingredients controller class, added UPDATE method, added form

--- src/main/java/pl/mykitchen/mykitchen/contollers/IngredientController.java
+++ src/main/java/pl/mykitchen/mykitchen/contollers/IngredientController.java
@@ -1,30 +1,57 @@
 package pl.mykitchen.mykitchen.contollers;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
-import pl.mykitchen.mykitchen.commands.IngredientCommand;
-import pl.mykitchen.mykitchen.services.IngredientService;
+import pl.mykitchen.mykitchen.domain.Ingredient;
+import pl.mykitchen.mykitchen.repositories.IngredientRepository;
+
 @Slf4j
 @Controller
+@RequestMapping("/ingredients")
 public class IngredientController {
-    private final IngredientService ingredientService;
+    private final IngredientRepository ingredientRepository;
 
-    public IngredientController(IngredientService ingredientService) {
-        this.ingredientService = ingredientService;
+    public IngredientController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
     }
 
-    @RequestMapping({"/ingredient", "/ingredients", "/showIngredients"})
+    @GetMapping()
     public String getIngredients(Model model) {
-        model.addAttribute("ingredients",ingredientService.getIngredients());
+        model.addAttribute(new Ingredient());
+        model.addAttribute("ingredients", ingredientRepository.findAll());
         return "ingredients";
     }
-    @RequestMapping(name = "ingredients")
-    public String saveIngredient(@ModelAttribute IngredientCommand command) {
-        IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command);
-        return "redirect:/ingredients" + savedCommand.getId();
+
+    @PostMapping()
+    @RequestMapping("/addIngredient")
+    public String addIngredient(Ingredient ingredient) {
+        ingredientRepository.save(ingredient);
+        return "redirect:/ingredients";
+    }
+
+    @GetMapping()
+    @RequestMapping("/delete/{id}")
+    public String deleteIngredient(@PathVariable String id) {
+        log.debug("Deleting id: " + id);
+        ingredientRepository.deleteById(Long.valueOf(id));
+        return "redirect:/ingredients";
+    }
+
+    @GetMapping("/update/{id}")
+    public String getAllEmployees(Model model, @PathVariable("id") Long id, Ingredient ingredient) {
+        model.addAttribute("editIngredient", ingredientRepository.findById(id).get());
+        return "update-ingredient-form";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateEmployee(@ModelAttribute("editIngredient") Ingredient ingredient, @PathVariable("id") Long ingredientId,
+                                 Model model) {
+        ingredientRepository.save(ingredient).setId(ingredientId);
+        model.addAttribute("ingredients", ingredientRepository.findAll());
+        return "ingredients";
     }
 }
--- src/main/java/pl/mykitchen/mykitchen/repositories/IngredientRepository.java
+++ src/main/java/pl/mykitchen/mykitchen/repositories/IngredientRepository.java
@@ -1,7 +1,11 @@
 package pl.mykitchen.mykitchen.repositories;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import pl.mykitchen.mykitchen.domain.Ingredient;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
-}
+@Repository
+public interface IngredientRepository extends CrudRepository<Ingredient, Long> {}
