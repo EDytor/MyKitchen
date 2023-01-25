9e7b678d2ff690b620a1b4ecc47d189c85148b80
EDyTor <91157109+EDyTor@users.noreply.github.com>
2023-01-25 15:43:10 +0100

    ShoppingListItem - domain, service, implementation, repository, controller

--- src/main/java/pl/mykitchen/mykitchen/domain/Ingredient.java
+++ src/main/java/pl/mykitchen/mykitchen/domain/Ingredient.java
@@ -16,7 +16,6 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-    private BigDecimal amount;
     @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
 
@@ -25,14 +24,12 @@ public class Ingredient {
 
     public Ingredient() {
     }
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+    public Ingredient(String description, UnitOfMeasure uom) {
         this.description = description;
-        this.amount = amount;
         this.uom = uom;
     }
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    public Ingredient(String description, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
-        this.amount = amount;
         this.uom = uom;
         this.recipe = recipe;
     }
